fb619a5148e901c2c8737823aa4179d49a869d25
omkar2781 <omkardkamble70.com>
2024-12-30 16:39:05 +0530

    fifth

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "webserver" {
+resource "aws_instance" {
   count         = 2
   ami           = "ami-064519b8c76274859"  # Replace with a valid AMI ID for your region
   instance_type = "t2.micro"
