85ea3aec563f3dd6d6c99ba8702fad0ccd44e354
omkar2781 <omkardkamble70.com>
2024-12-30 16:20:41 +0530

    fifth

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "webserver" {
+  count         = 2
+  ami           = "ami-064519b8c76274859"  # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "webserver-instance-${count.index + 1}"
+  }
+}
--- provider.tf
+++
@@ -1,8 +0,0 @@
-# Configure the AWS Provider
-provider "aws" {
-  region = "ap-south-1"
-  /*
-  access_key = "AKIAYKFQQTXKVP4EBYWN"
-  secret_key = "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"
-  */
-}
