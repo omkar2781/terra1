95fd5bac20bf180df53c6ad66486da1bea888ff6
omkar2781 <omkardkamble70.com>
2024-12-30 14:35:46 +0530

    fourth chnage terrafom

---
+++ provider.tf
@@ -0,0 +1,8 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+  /*
+  access_key = "AKIAYKFQQTXKVP4EBYWN"
+  secret_key = "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"
+  */
+}
